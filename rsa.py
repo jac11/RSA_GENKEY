345273b9304f0a670c07007fc5b4b19477dedc55
jac11 <jac11devel@gmail.com>
2023-05-26 03:27:50 +0300

    Add files via upload

--- rsa.py
+++ rsa.py
@@ -1,5 +1,16 @@
+#d = (e^-1) mod phi(n)
+#C = (M^e) mod n = 31^7 mod 33 = 4
+#: M = (C^d) mod n = 4^3 mod 33 = 31
+# 1<e<phi(n) and gcd(phi(n), e) = 1 
+
 import random
 import math
+import base64
+list_Str = []
+string  = "Welcome Back Jacstory to Home 23"
+for L in string :
+    Num_str = ord(L)
+    list_Str.append(Num_str)
 list =[
         1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049, 1051,
         1061, 1063, 1069, 1087, 1091, 1093, 1097, 1103, 1109,
@@ -17,44 +28,46 @@ list =[
         1871, 1873, 1877, 1879, 1889, 1901, 1907, 1913, 1931,
         1933, 1949, 1951, 1973, 1979, 1987, 1993, 1997, 1999
      ]
-p = random.choice(list)
-q = random.choice(list)
-liste = []
-if p == q:
-	p = random.choice(list)
-n = p*q
-QN = (p-1)*(q-1)
-for e in range(int(QN)):
-    if 1< e < QN:
-        if math.gcd(QN,e) == 1:
-            if e == p :
-           	  continue
-            else:
-            	liste.append(e)
-e = random.choice(liste)
-for d in range(QN):
-    if (d*e)% QN ==1 :
-       if d == e :
-           d +=1
-       else:    
-	        break
-m = 500
-C = (m**e)%n 
-D = (C**d)%n
-
-print("M  : " ,m)
-print("p  : ", p)
-print("q  : ", q)
-print("n  : ", n)
-print("QN : " ,QN)
-print("e  : ", e)
-print("d  : ",d)
-print("C  : ", C)
-print("D  : " ,D)  	 
-
-#d = (e^-1) mod phi(n)
-#print(list)
-#exit()
-#C = (M^e) mod n = 31^7 mod 33 = 4
-#: M = (C^d) mod n = 4^3 mod 33 = 31
-# 1<e<phi(n) and gcd(phi(n), e) = 1
+class RSA_algorithm:
+    def __init__(self):
+        self.algorithm()
+    def algorithm (self):
+        p = random.choice(list)
+        q = random.choice(list)
+        liste = []
+        if p == q:
+           p = random.choice(list)
+        n = p*q
+        QN = (p-1)*(q-1)
+        for e in range(int(QN)):
+            if 1< e < QN:
+               if math.gcd(QN,e) == 1:
+                    if e == p :
+           	           continue
+                    else:
+            	        liste.append(e)
+        e = random.choice(liste)
+        for d in range(QN):
+            if (d*e)% QN ==1 :
+                if d == e :
+                  d +=1
+                else:    
+                   break        
+        list_ency = []
+        list_chr = []
+        for m in list_Str :
+            C = (m**e)%n 
+            list_ency.append(C)
+        encrypit =str("".join(str(list_ency))).replace("[",'').replace(',','').replace("]",'') 
+        enbase64 = str(base64.b64encode(bytes(encrypit, 'utf-8'))).replace("b'",'').replace("'",'') 
+        Debaes64 =base64.b64decode(enbase64).decode("utf-8").split(" ")
+        for S in Debaes64 :
+            S = int(S)
+            D = (S**d)%n
+            chr_c = chr(D)
+            list_chr.append(chr_c)
+        text = "".join(list_chr)
+        print("C  : ", enbase64)
+        print("Massage : ",text)	 
+if  __name__ == '__main__':
+    RSA_algorithm()
