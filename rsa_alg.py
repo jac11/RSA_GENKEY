10fc028f0ce4e08404d77fb1a1b7b03be0220ca3
jac11 <jac11devel@gmail.com>
2023-07-09 13:27:30 -0400

    Add files via upload

--- rsa_alg.py
+++ rsa_alg.py
@@ -197,8 +197,8 @@ class RSA_algorithm:
                 newpath.write(str("/".join(pbkey.split("/")[:-1])))      
             with open(".path2",'w') as path2:
                 path2.write(pbkey)
-       # except Exception as E:
-        #    print("🚨️🚧️  Error  : ::------------::".strip(),E)  
+        except Exception as E:
+            print("🚨️🚧️  Error  : ::------------::".strip(),E)  
         except KeyboardInterrupt :
             print("🚨️🚧️  Error ::------------: KeyboardInterrupt".strip())    
             exit()                          
